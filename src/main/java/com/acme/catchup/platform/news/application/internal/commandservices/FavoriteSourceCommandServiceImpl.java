4254d3087dd45ae26f333296b2f0f50d460848ad
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-18 18:31:31 -0500

    docs: added application layer documentation.

--- src/main/java/com/acme/catchup/platform/news/application/internal/commandservices/FavoriteSourceCommandServiceImpl.java
+++ src/main/java/com/acme/catchup/platform/news/application/internal/commandservices/FavoriteSourceCommandServiceImpl.java
@@ -1,3 +1,11 @@
+/**
+ * FavoriteSourceCommandServiceImpl.java
+ * @summary
+ * The FavoriteSourceCommandServiceImpl class is an implementation of the FavoriteSourceCommandService interface.
+ * It provides the implementation for the handle method, which creates a new FavoriteSource object and saves it to the database.
+ * @author
+ * { author.name } { author.email }
+ */
 package com.acme.catchup.platform.news.application.internal.commandservices;
 
 import com.acme.catchup.platform.news.domain.model.aggregates.FavoriteSource;
@@ -8,6 +16,12 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+/**
+ * FavoriteSourceCommandServiceImpl
+ * @summary
+ * FavoriteSourceCommandServiceImpl is an implementation of the FavoriteSourceCommandService interface.
+ * It provides the implementation for the handle method, which creates a new FavoriteSource object and saves it to the database.
+ */
 @Service
 public class FavoriteSourceCommandServiceImpl implements FavoriteSourceCommandService {
     private final FavoriteSourceRepository favoriteSourceRepository;
@@ -16,6 +30,11 @@ public class FavoriteSourceCommandServiceImpl implements FavoriteSourceCommandSe
         this.favoriteSourceRepository = favoriteSourceRepository;
     }
 
+    /**
+     * CreateFavoriteSourceCommand handler
+     * @param command CreateFavoriteSourceCommand the command to handle including the newsApiKey and sourceId of the favorite source
+     * @return Optional<FavoriteSource> the created favorite source
+     */
     @Override
     public Optional<FavoriteSource> handle(CreateFavoriteSourceCommand command) {
         if (favoriteSourceRepository.existsByNewsApiKeyAndSourceId(command.newsApiKey(), command.sourceId()))
--- src/main/java/com/acme/catchup/platform/news/application/internal/queryservices/FavoriteSourceQueryServiceImpl.java
+++ src/main/java/com/acme/catchup/platform/news/application/internal/queryservices/FavoriteSourceQueryServiceImpl.java
@@ -1,3 +1,11 @@
+/**
+ * FavoriteSourceQueryServiceImpl
+ * @summary
+ * FavoriteSourceQueryServiceImpl is an implementation of the FavoriteSourceQueryService interface.
+ * It provides the implementation for the handle method, which retrieves favorite sources from the database.
+ * @author
+ * { author.name } { author.email }
+ */
 package com.acme.catchup.platform.news.application.internal.queryservices;
 
 import com.acme.catchup.platform.news.domain.model.aggregates.FavoriteSource;
@@ -11,6 +19,13 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * FavoriteSourceQueryServiceImpl
+ * @summary
+ * FavoriteSourceQueryServiceImpl is an implementation of the FavoriteSourceQueryService interface.
+ * It provides the implementation for the handle method, which retrieves favorite sources from the database.
+ *
+ */
 @Service
 public class FavoriteSourceQueryServiceImpl implements FavoriteSourceQueryService {
     private final FavoriteSourceRepository favoriteSourceRepository;
@@ -19,16 +34,31 @@ public class FavoriteSourceQueryServiceImpl implements FavoriteSourceQueryServic
         this.favoriteSourceRepository = favoriteSourceRepository;
     }
 
+    /**
+     * GetFavoriteSourceByIdQuery handler
+     * @param query GetFavoriteSourceByIdQuery the query to handle including the id of the favorite source
+     * @return Optional<FavoriteSource> the favorite source with the given id
+     */
     @Override
     public Optional<FavoriteSource> handle(GetFavoriteSourceByIdQuery query) {
         return favoriteSourceRepository.findById(query.id());
     }
 
+    /**
+     * GetFavoriteSourceByNewsApiKeyAndSourceIdQuery handler
+     * @param query GetFavoriteSourceByNewsApiKeyAndSourceIdQuery the query to handle including the newsApiKey and sourceId of the favorite source
+     * @return Optional<FavoriteSource> the favorite source with the given newsApiKey and sourceId
+     */
     @Override
     public Optional<FavoriteSource> handle(GetFavoriteSourceByNewsApiKeyAndSourceIdQuery query) {
         return favoriteSourceRepository.findByNewsApiKeyAndSourceId(query.newsApiKey(), query.sourceId());
     }
 
+    /**
+     * GetAllFavoriteSourcesByNewsApiKeyQuery handler
+     * @param query GetAllFavoriteSourcesByNewsApiKeyQuery the query to handle including the newsApiKey of the favorite sources
+     * @return List<FavoriteSource> the list of favorite sources with the given newsApiKey
+     */
     @Override
     public List<FavoriteSource> handle(GetAllFavoriteSourcesByNewsApiKeyQuery query) {
         return favoriteSourceRepository.findAllByNewsApiKey(query.newsApiKey());
