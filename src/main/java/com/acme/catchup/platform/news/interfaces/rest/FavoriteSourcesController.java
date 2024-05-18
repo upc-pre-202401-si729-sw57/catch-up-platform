e334e842cc510d551100f490c9a81b35c76cf53f
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-18 17:27:18 -0500

    feat(favorite-sources): added application configuration.

--- src/main/java/com/acme/catchup/platform/news/interfaces/rest/FavoriteSourcesController.java
+++ src/main/java/com/acme/catchup/platform/news/interfaces/rest/FavoriteSourcesController.java
@@ -1,17 +1,20 @@
 package com.acme.catchup.platform.news.interfaces.rest;
 
 import com.acme.catchup.platform.news.domain.model.aggregates.FavoriteSource;
+import com.acme.catchup.platform.news.domain.model.queries.GetAllFavoriteSourcesByNewsApiKeyQuery;
+import com.acme.catchup.platform.news.domain.model.queries.GetFavoriteSourceByIdQuery;
+import com.acme.catchup.platform.news.domain.model.queries.GetFavoriteSourceByNewsApiKeyAndSourceIdQuery;
 import com.acme.catchup.platform.news.domain.services.FavoriteSourceCommandService;
+import com.acme.catchup.platform.news.domain.services.FavoriteSourceQueryService;
 import com.acme.catchup.platform.news.interfaces.rest.resources.CreateFavoriteSourceResource;
 import com.acme.catchup.platform.news.interfaces.rest.resources.FavoriteSourceResource;
 import com.acme.catchup.platform.news.interfaces.rest.transform.CreateFavoriteSourceCommandFromResourceAssembler;
 import com.acme.catchup.platform.news.interfaces.rest.transform.FavoriteSourceResourceFromEntityAssembler;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.springframework.http.HttpStatus.CREATED;
@@ -20,9 +23,11 @@ import static org.springframework.http.HttpStatus.CREATED;
 @RequestMapping("/api/v1/favorite-sources")
 public class FavoriteSourcesController {
     private FavoriteSourceCommandService favoriteSourceCommandService;
+    private FavoriteSourceQueryService favoriteSourceQueryService;
 
-    public FavoriteSourcesController(FavoriteSourceCommandService favoriteSourceCommandService) {
+    public FavoriteSourcesController(FavoriteSourceCommandService favoriteSourceCommandService, FavoriteSourceQueryService favoriteSourceQueryService) {
         this.favoriteSourceCommandService = favoriteSourceCommandService;
+        this.favoriteSourceQueryService = favoriteSourceQueryService;
     }
 
     @PostMapping
@@ -32,5 +37,41 @@ public class FavoriteSourcesController {
         return favoriteSource.map(source -> new ResponseEntity<>(
                 FavoriteSourceResourceFromEntityAssembler.toResourceFromEntity(source), CREATED))
                 .orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<FavoriteSourceResource> getFavoriteSourceById(@PathVariable Long id) {
+        var getFavoriteSourceByIdQuery = new GetFavoriteSourceByIdQuery(id);
+        Optional<FavoriteSource> favoriteSource = favoriteSourceQueryService.handle(getFavoriteSourceByIdQuery);
+        if (favoriteSource.isEmpty()) return ResponseEntity.notFound().build();
+        return favoriteSource.map(source -> ResponseEntity.ok(FavoriteSourceResourceFromEntityAssembler.toResourceFromEntity(source)))
+                .orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+    private ResponseEntity<List<FavoriteSourceResource>> getAllFavoriteSourcesByNewsApiKey(String newsApiKey) {
+        var getAllFavoriteSourcesByNewsApiKeyQuery = new GetAllFavoriteSourcesByNewsApiKeyQuery(newsApiKey);
+        List<FavoriteSource> favoriteSources = favoriteSourceQueryService.handle(getAllFavoriteSourcesByNewsApiKeyQuery);
+        if (favoriteSources.isEmpty()) return ResponseEntity.notFound().build();
+        var favoriteSourceResources = favoriteSources.stream().map(FavoriteSourceResourceFromEntityAssembler::toResourceFromEntity).toList();
+        return ResponseEntity.ok(favoriteSourceResources);
+    }
+
+    private ResponseEntity<FavoriteSourceResource> getFavoriteSourceByNewsApiKeyAndSourceId(String newsApiKey, String sourceId) {
+        var getFavoriteSourceByNewsApiKeyAndSourceIdQuery = new GetFavoriteSourceByNewsApiKeyAndSourceIdQuery(newsApiKey, sourceId);
+        Optional<FavoriteSource> favoriteSource = favoriteSourceQueryService.handle(getFavoriteSourceByNewsApiKeyAndSourceIdQuery);
+        if (favoriteSource.isEmpty()) return ResponseEntity.notFound().build();
+        return favoriteSource.map(source -> ResponseEntity.ok(FavoriteSourceResourceFromEntityAssembler.toResourceFromEntity(source)))
+                .orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getFavoriteSourcesWithParameters(@RequestParam Map<String, String> params) {
+        if (params.containsKey("newsApiKey") && params.containsKey("sourceId")) {
+            return getFavoriteSourceByNewsApiKeyAndSourceId(params.get("newsApiKey"), params.get("sourceId"));
+        } else if (params.containsKey("newsApiKey")) {
+            return getAllFavoriteSourcesByNewsApiKey(params.get("newsApiKey"));
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
